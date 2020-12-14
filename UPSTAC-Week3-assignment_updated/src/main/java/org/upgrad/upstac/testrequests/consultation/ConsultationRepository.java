69fd659e476d49e4e79a32ab506d34ac5d5c965d
anandgourav <anand.gourav@gmail.com>
2020-12-14 20:32:15 +0530

    project added

---
+++ UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationRepository.java
@@ -0,0 +1,32 @@
+package org.upgrad.upstac.testrequests.consultation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.upgrad.upstac.testrequests.RequestStatus;
+import org.upgrad.upstac.testrequests.TestRequest;
+import org.upgrad.upstac.users.User;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface ConsultationRepository extends JpaRepository<Consultation,Long> {
+
+
+    Optional<Consultation> findById(Long id);
+
+
+
+    void deleteById(Long id);
+
+
+
+
+
+    List<Consultation> findByDoctor(RequestStatus user);
+    Optional<Consultation> findByRequest(TestRequest testRequest);
+
+    Optional<Consultation> findByDoctorAndRequest(User doctor,TestRequest testRequest);
+
+
+}
